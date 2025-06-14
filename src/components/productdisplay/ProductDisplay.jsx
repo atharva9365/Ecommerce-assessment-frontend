125305bdea47b6bc82e3de972ee199a4098ffbbb
atharva9365 <kolteatharva1@gmail.com>
2025-06-14 12:38:50 +0530

    modified

--- src/components/productdisplay/ProductDisplay.jsx
+++ src/components/productdisplay/ProductDisplay.jsx
@@ -51,13 +51,7 @@ const ProductDisplay = (props) => {
                          </div>
                           <div className="productdisplay-right-size">
                               <h1>Select Size</h1>
-                               <div className='productdisplay-right-sizes'>
-                                   <div>S</div>
-                                   <div>M</div>
-                                   <div>L</div>
-                                   <div>XL</div>
-                                   <div>XXL</div>
-                               </div>
+                            
                           </div>
                           <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
                           <p className='productdisplay-right-category'><span>Category :</span> Women, T-shirt, Crop Top</p>
